e09106c47bbdb0d037d5dfec6fc0f797b6c263bc
Kevin Laptop <dev4@bankedge.com>
2018-11-28 16:03:10 +1000

    web speech implementation successful

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,61 +1,115 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SpeechRecognition from 'react-speech-recognition';
-import PropTypes from 'prop-types';
 
-//initialisation of web speech api
-// const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+//initialisation of web speech api for Google Chrome only
+const SpeechRecog = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+const recognition= new SpeechRecog();
 
-const hello= <h1>Hello World</h1>
+//changing the default properties of speech recognition
+recognition.continous=true;
+recognition.interimResults= true;
+recognition.lang='en-US'
 
-const propTypes ={
-    transcript: PropTypes.string,
-    resetTranscript: PropTypes.func,
-    browserSupportsSpeechRecognition: PropTypes.bool,
-    startListening: PropTypes.func
-}
-class Dictaphone extends React.Component {
-    render() {
-      const { transcript, resetTranscript, browserSupportsSpeechRecognition , startListening } = this.props
-  
-    //   if (!browserSupportsSpeechRecognition) {
-    //     return null
-    //   }
-  
-      return (
-        <div>
-          <button onClick={startListening}>Reset</button>
-          <span>{transcript}</span>
-        </div>
-      )
-    }
-}
 
-Dictaphone.propTypes=propTypes
-
-export default SpeechRecognition(Dictaphone)
-
+// Component to Record Speech
 class Recording extends React.Component{
     constructor(props){
         super(props)
-        this.state={message:"Click to Start",active:false}
-        this.handleClick= this.handleClick.bind(this)
+        this.state={message:"Click to Start",listening:false}
+        // this.handleClick= this.handleClick.bind(this)
+        this.toggleListen= this.toggleListen.bind(this)
+        this.handleListen = this.handleListen.bind(this)
     }
 
-    handleClick(){
-        this.setState({message:"Recording",active: !this.state.active},()=>{
-            if(!this.state.active){
-                this.setState({message:"Click to Start"})
-            }
-        })
+    componentDidMount(){
+        console.log(recognition);
     }
+
+    //toggling function to initiate speech recognition. handleListen is called in callback to process the speech recognition logic
+    toggleListen(){
+        console.log("Hello")
+        this.setState({
+            //stop listening when button is pressed
+            listening: !this.state.listening
+        },() => {this.handleListen()})
+
+    }
+
+    //speech recognition logic
+    handleListen(){
+        console.log('listening?', this.state.listening)
+
+        if(this.state.listening){
+            recognition.start()
+            recognition.onend=()=> {
+                console.log("conitue listening")
+                recognition.start()
+            }
+        }
+        else{
+            //press button to stop recording
+            console.log('stop')
+            recognition.stop()
+            recognition.onend=() =>{
+                console.log("stopped listening per click")
+            }
+
+        }
+
+        recognition.onstart= ()=>{
+            console.log("Listening")
+        }
+        //getting the words from speech recognition
+        let finalTranscript=''
+        recognition.onresult= event => {
+            let interimTranscript=''
+            //the following for loop is based from the documentation provided by MDN
+            for(let i= event.resultIndex; i<event.results.length;i++){
+                const transcript = event.results[i][0].transcript;
+                if(event.results[i].isFinal){
+                    finalTranscript+= transcript +=' ';
+                }
+                else{
+                    interimTranscript += transcript;
+                }
+            }
+            //putting the words in the respective div
+            document.getElementById('interim').innerHTML = interimTranscript;
+            document.getElementById('final').innerHTML=finalTranscript;
+            //listening to 'Stop listening' command to stop recognition
+            const transcriptArr= finalTranscript.split(' ')
+            const stopCmd = transcriptArr.slice(-3,-1)
+            console.log('stopCmd',stopCmd)
+
+            if(stopCmd[0] == 'stop' && stopCmd[1] === 'listening'){
+                recognition.stop()
+                recognition.onend = () =>{
+                    console.log("stop listening per command")
+                    const finalText = transcriptArr.slice(0,-3).join(' ')
+                    document.getElementById('final').innerHTML = finalText
+                }
+            }
+        }
+        //checking whether there are any errors during recognition
+        recognition.onerror = event =>{
+            console.log("Error occured in recognition: " + event.error)
+        }
+
+        
+    }
+
 
     render(){
         return(
             <div>
-                <h1>{this.state.message}</h1>
-                <button className="btn btn-primary" onClick={this.handleClick}>{this.state.active?"Stop":"Record"}</button>
+                {/* <h1>{this.state.message}</h1> */}
+                {/* {this.state.active?"Stop":"Record"} */}
+                <button className="btn btn-primary" onClick={this.toggleListen}>Press to Record</button>
+                <div id="final"></div>
+                <div id="interim"></div>
             </div>
             
         )
@@ -63,11 +117,11 @@ class Recording extends React.Component{
 }
 
 ReactDOM.render(
-    <Dictaphone />,
+    <Recording />,
     document.getElementById('root')
 )
 
-ReactDOM.render(
-    hello,
-    document.getElementById('sub')
-)
+// ReactDOM.render(
+//     <Dictaphone/>,
+//     document.getElementById('sub')
+// )
