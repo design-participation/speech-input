a5df49998bbbee08e05b3581b3d3aeb2bdee2758
Kevin Laptop <dev4@bankedge.com>
2018-12-03 12:32:06 +1000

    bing speech api

---
+++ my-app/src/WindowsRecording.js
@@ -0,0 +1,3 @@
+import { createFetchTokenUsingSubscriptionKey, SpeechGrammarList, SpeechRecognition } from 'web-speech-cognitive-services';
+import DictateButton from 'react-dictate-button';
+import React from 'react';
