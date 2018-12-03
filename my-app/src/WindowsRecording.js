1c1bda7003ac3ef8c92a401b772ec8835892cf33
Kevin Laptop <dev4@bankedge.com>
2018-12-03 12:50:27 +1000

    windows speech implementation part 1

--- my-app/src/WindowsRecording.js
+++ my-app/src/WindowsRecording.js
@@ -1,3 +1,6 @@
 import { createFetchTokenUsingSubscriptionKey, SpeechGrammarList, SpeechRecognition } from 'web-speech-cognitive-services';
 import DictateButton from 'react-dictate-button';
 import React from 'react';
+import token from './secrets'
+
+console.log(token);
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './GoogleRecording'
+import Window from './secrets'
+
+console.log(Window)
 
 ReactDOM.render(
     <App />,
